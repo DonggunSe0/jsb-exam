d4a985ddf09195a503efb8f260f2969fce0a32df
DonggunSe0 <cloudhenry1@gmail.com>
2026-02-27 17:11:21 +0900

    5

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    @ResponseBody //브라우저에 표시하도록 응답 본문에 직접 문자열을 반환하도록 지정
+    public String index() {
+        return "sbb";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello";
+
+    }
+}
---
+++ src/test/java/com/example/demo/JsbExamApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test") // 테스트 실행 시 "test" 프로파일을 활성화하여 application-test.properties 파일을 사용하도록 지정
+class JsbExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
