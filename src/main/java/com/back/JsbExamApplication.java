abe1802cc471ac052177b601d6b996bfa01b4a16
DonggunSe0 <cloudhenry1@gmail.com>
2026-03-02 18:38:31 +0900

    8 onetomany manytoone

--- src/main/java/com/back/JsbExamApplication.java
+++ src/main/java/com/back/JsbExamApplication.java
@@ -7,6 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JsbExamApplication {
 
 	public static void main(String[] args) {
+		//답글로 부터 질문 객체를 가져오는 방법
+//		Answer a = new Answer();
+//		a.getQuestion(); // Answer 객체에서 Question 객체를 가져오는 메서드 호출
+//		Question question = a.getQuestion();  //
+//		question.getContent(); //
+//
+//		a.getQuestion().getContent();
+//		//질문을 조회하는 방법
+//		Question q = new Question();
+//		List<Answer> answers = q.getAnswers();
+
+
 		SpringApplication.run(JsbExamApplication.class, args);
 	}
 
